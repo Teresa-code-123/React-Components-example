b2820132cb8b5b7295e0e24aa3c4330df8c4e82b
marie-hotchkiss <mtknowlton67@gmail.com>
2020-02-02 15:34:30 -0800

    Added PostsPage.js

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,13 +4,22 @@ import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  console.log("props".props)
   return (
     <div className="posts-container-wrapper">
+      {props.postData.map(pData => (
+        <Post
+          key = {pData.imageUrl}
+          post2 = {pData}
+        />
+      ))}
       {/* map through data here */}
+
     </div>
   );
 };
 
 export default PostsPage;
+
 
