f56d4bb97b0fb1dbca36eda3c421fbbda9919459
Marie Hotchkiss <mtknowlton67@gmail.com>
2022-03-03 15:07:52 -0800

    Added the comment section and the posts section

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,16 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = (props) => {
   // Add state for the comments
-
+  const [comments] = useState(props.comments);
+  console.log("This is from Components/ CommnetSectionContainer", props);
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((c, index) => (
+        <Comment key={index} comment={c} />
+      ))}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,22 +1,20 @@
 // You will add code in this file
 
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
 
 import "./Posts.css";
 
-const Post = props => {
+const Post = (props) => {
   // set up state for the likes
-
+  const [likes, setLikes] = useState(props.post.likes);
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
